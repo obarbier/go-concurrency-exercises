e49b6d489b96f5bfa37d46e3c356ac5a1479aba5
obarbier <obarbier13@gmail.com>
2021-07-02 18:36:34 -0400

    my way

--- 02-exercise/02-pipeline/01-pipeline/main.go
+++ 02-exercise/02-pipeline/01-pipeline/main.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 // TODO: Build a Pipeline
 // generator() -> square() -> print
+var ch1, ch2 chan int
 
 // generator - convertes a list of integers to a channel
 func generator(nums ...int) {
+	// ch1 = make(chan int)
+	for num := range nums {
+		ch1 <- num
+	}
+	close(ch1)
 
 }
 
@@ -12,12 +20,22 @@ func generator(nums ...int) {
 // square the number
 // output on outbound channel
 func square() {
-
+	// ch2 = make(chan int)
+	for val := range ch1 {
+		ch2 <- val * val
+	}
+	close(ch2)
 }
 
 func main() {
+	ch1, ch2 = make(chan int), make(chan int)
 	// set up the pipeline
+	go generator(1, 2, 3, 4, 5, 6, 7)
+	go square()
+	for i := range ch2 {
+		fmt.Println(i)
 
+	}
 	// run the last stage of pipeline
 	// receive the values from square stage
 	// print each one, until channel is closed.
