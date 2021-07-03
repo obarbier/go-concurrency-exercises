87a6aab7fb0c54719001498a6c6f78ac2835c02f
obarbier <obarbier13@gmail.com>
2021-07-02 21:51:57 -0400

    wow better way for 02-pipeline

--- 02-exercise/02-pipeline/02-pipeline/main.go
+++ 02-exercise/02-pipeline/02-pipeline/main.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func generator(nums ...int) <-chan int {
 	out := make(chan int)
@@ -17,32 +20,30 @@ func generator(nums ...int) <-chan int {
 }
 
 func square(in <-chan int) <-chan int {
-	out1 := make(chan int)
-	out2 := make(chan int)
+	out := make(chan int)
 	go func() {
 		for n := range in {
-			out1 <- n * n
+			out <- n * n
 		}
-		close(out1)
+		close(out)
 	}()
-	go func() {
-		for n := range in {
-			out2 <- n * n
-		}
-		close(out2)
-	}()
-	return merge(out1, out2)
+	return merge(out)
 }
 
 func merge(cs ...<-chan int) <-chan int {
 	out := make(chan int)
-	go func() {
-		for _, c := range cs {
+	var wg sync.WaitGroup
+	for _, c := range cs {
+		wg.Add(1)
+		go func(c <-chan int) {
+			defer wg.Done()
 			for i := range c {
 				out <- i
 			}
-
-		}
+		}(c)
+	}
+	go func() {
+		wg.Wait()
 		close(out)
 	}()
 	return out
@@ -52,10 +53,11 @@ func main() {
 	in := generator(2, 3, 7, 8, 9, 13, 221)
 
 	// fan out square stage to run two instances.
-	in2 := square(in)
+	ch2a := square(in)
+	ch2b := square(in)
 
 	// TO fan in the results of square stages.
-	for c := range in2 {
+	for c := range merge(ch2a, ch2b) {
 		fmt.Println(c)
 	}
 
