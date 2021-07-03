00e3ca89a472644ed8ea616e0301dc43396c5a2c
obarbier <obarbier13@gmail.com>
2021-07-02 21:37:52 -0400

    02-pipeline my solution

--- 02-exercise/02-pipeline/02-pipeline/main.go
+++ 02-exercise/02-pipeline/02-pipeline/main.go
@@ -2,6 +2,8 @@
 
 package main
 
+import "fmt"
+
 func generator(nums ...int) <-chan int {
 	out := make(chan int)
 
@@ -15,26 +17,46 @@ func generator(nums ...int) <-chan int {
 }
 
 func square(in <-chan int) <-chan int {
-	out := make(chan int)
+	out1 := make(chan int)
+	out2 := make(chan int)
 	go func() {
 		for n := range in {
-			out <- n * n
+			out1 <- n * n
+		}
+		close(out1)
+	}()
+	go func() {
+		for n := range in {
+			out2 <- n * n
+		}
+		close(out2)
+	}()
+	return merge(out1, out2)
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, c := range cs {
+			for i := range c {
+				out <- i
+			}
+
 		}
 		close(out)
 	}()
 	return out
 }
 
-func merge(cs ...<-chan int) <-chan int {
-	// Implement fan-in
-	// merge a list of channels to a single channel
-}
-
 func main() {
-	in := generator(2, 3)
+	in := generator(2, 3, 7, 8, 9, 13, 221)
 
-	// TODO: fan out square stage to run two instances.
+	// fan out square stage to run two instances.
+	in2 := square(in)
 
-	// TODO: fan in the results of square stages.
+	// TO fan in the results of square stages.
+	for c := range in2 {
+		fmt.Println(c)
+	}
 
 }
