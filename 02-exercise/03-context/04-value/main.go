6af8053cf85b9e599645337acc704c24780a1bd5
obarbier <obarbier13@gmail.com>
2021-07-05 10:49:24 -0400

    context withvalue

--- 02-exercise/03-context/04-value/main.go
+++ 02-exercise/03-context/04-value/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 )
 
@@ -10,6 +11,8 @@ var db database = database{
 	"jane": true,
 }
 
+type userIdType string
+
 func main() {
 	processRequest("jane")
 }
@@ -17,7 +20,8 @@ func main() {
 func processRequest(userid string) {
 	// TODO: send userID information to checkMemberShip through context for
 	// map lookup.
-	ch := checkMemberShip()
+	ctx := context.WithValue(context.Background(), string("userid"), userid)
+	ch := checkMemberShip(ctx)
 	status := <-ch
 	fmt.Printf("membership status of userid : %s : %v\n", userid, status)
 }
@@ -26,11 +30,12 @@ func processRequest(userid string) {
 // extracts the user id information from context.
 // spins a goroutine to do map lookup
 // sends the result on the returned channel.
-func checkMemberShip() <-chan bool {
+func checkMemberShip(ctx context.Context) <-chan bool {
 	ch := make(chan bool)
 	go func() {
 		defer close(ch)
 		// do some database lookup
+		userid := ctx.Value(string("userid")).(string)
 		status := db[userid]
 		ch <- status
 	}()
