971af8a94978f413daef9b55ea87bad388f85988
obarbier <obarbier13@gmail.com>
2021-07-05 10:29:35 -0400

    context timeout mine

--- 02-exercise/03-context/03-timeout/main.go
+++ 02-exercise/03-context/03-timeout/main.go
@@ -1,30 +1,57 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 
-	// TODO: set a http client timeout
+	// set a http client timeout
 
-	req, err := http.NewRequest("GET", "https://andcloud.io", nil)
-	if err != nil {
-		log.Fatal(err)
+	duration := 10 * time.Millisecond
+
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+	process := func(ctx context.Context) <-chan *http.Response {
+		out := make(chan *http.Response)
+
+		go func() {
+			defer close(out)
+			req, err := http.NewRequest("GET", "https://andcloud.io", nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Println("ERROR:", err)
+				return
+			}
+
+			// Write the response to stdout.
+			select {
+			case <-ctx.Done():
+				fmt.Println("Timeout")
+				resp.Body.Close()
+				return
+			case out <- resp:
+			}
+
+		}()
+
+		return out
+
 	}
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println("ERROR:", err)
-		return
+	c, ok := <-process(ctx)
+	if ok {
+		io.Copy(os.Stdout, c.Body)
+		c.Body.Close()
 	}
-
-	// Close the response body on the return.
-	defer resp.Body.Close()
-
-	// Write the response to stdout.
-	io.Copy(os.Stdout, resp.Body)
 }
