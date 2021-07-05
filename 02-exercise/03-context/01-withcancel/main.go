46ec9f0b95cef1742ba6687f2bcb37b7a14608ef
obarbier <obarbier13@gmail.com>
2021-07-05 10:29:21 -0400

    contextxercise

--- 02-exercise/03-context/01-withcancel/main.go
+++ 02-exercise/03-context/01-withcancel/main.go
@@ -1,17 +1,49 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"runtime"
+)
+
 func main() {
 
-	// TODO: generator -  generates integers in a separate goroutine and
+	// generator -  generates integers in a separate goroutine and
 	// sends them to the returned channel.
 	// The callers of gen need to cancel the goroutine once
 	// they consume 5th integer value
 	// so that internal goroutine
 	// started by gen is not leaked.
-	generator := func() <-chan int {
+	generator := func(c context.Context) <-chan int {
+		out := make(chan int)
+		go func() {
+			defer close(out)
+			n := 0
+			for {
+				select {
+				case out <- n:
+					n++
+				case <-c.Done():
+					return
 
+				}
+			}
+		}()
+		return out
 	}
 
 	// Create a context that is cancellable.
+	ctx, cancel := context.WithCancel(context.Background())
 
+	count := 1
+	for c := range generator(ctx) {
+		fmt.Println(c)
+		count++
+
+		if count == 5 {
+			cancel()
+		}
+	}
+
+	fmt.Printf("number of running goroutine: %d", runtime.NumGoroutine())
 }
--- 02-exercise/03-context/02-withdeadline/main.go
+++ 02-exercise/03-context/02-withdeadline/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"golang.org/x/net/context"
 )
 
 type data struct {
@@ -12,23 +14,42 @@ type data struct {
 func main() {
 
 	// TODO: set deadline for goroutine to return computational result.
+	deadline := time.Now().Add(10 * time.Millisecond)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
 
-	compute := func() <-chan data {
+	compute := func(ctx context.Context) <-chan data {
 		ch := make(chan data)
 		go func() {
 			defer close(ch)
+
+			deadline, ok := ctx.Deadline()
+			if ok { // dealine is set
+				if deadline.Sub(time.Now().Add(50*time.Millisecond)) < 0 { // if the deadline wiill be less than 50 milisecond
+					fmt.Println("Not enough time to process this request")
+					return // return is important since the select statement can either do ch or ctx.Done()
+				}
+
+			}
 			// Simulate work.
 			time.Sleep(50 * time.Millisecond)
 
-			// Report result.
-			ch <- data{"123"}
+			// Report result.	
+			select {
+			case ch <- data{"123"}:
+			case <-ctx.Done():
+				return
+			}
+
 		}()
 		return ch
 	}
 
 	// Wait for the work to finish. If it takes too long move on.
-	ch := compute()
-	d := <-ch
-	fmt.Printf("work complete: %s\n", d)
+	ch := compute(ctx)
+	d, ok := <-ch
+	if ok { // if d is comming send operation
+		fmt.Printf("work complete: %s\n", d)
+	}
 
 }
