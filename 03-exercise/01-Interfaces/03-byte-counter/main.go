4cf9ac96a0b475bab538b04e5433d44ded83bc17
obarbier <obarbier13@gmail.com>
2021-07-07 19:54:41 -0400

    latest update

--- 03-exercise/01-Interfaces/03-byte-counter/main.go
+++ 03-exercise/01-Interfaces/03-byte-counter/main.go
@@ -7,7 +7,11 @@ type ByteCounter int
 
 // TODO: Implement Write method for ByteCounter
 // to count the number of bytes written.
-
+func (b *ByteCounter) Write(p []byte) (n int, err error) {
+	n = len(p)
+	*b = ByteCounter(n)
+	return n, nil
+}
 func main() {
 	var b ByteCounter
 	fmt.Fprintf(&b, "hello world")
